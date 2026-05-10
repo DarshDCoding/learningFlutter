da2f2190100a9c296483297130169a02084e4cc6
Darsh D. Coding <darshdcoding@gmail.com>
2026-05-11 00:41:35 +0530

    todo Ui, with whatever i know

--- lib/main.dart
+++ lib/main.dart
@@ -12,37 +12,130 @@ class App extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        // backgroundColor: Colors.deepPurple.shade200,
-        body: 
-        Container(
+        backgroundColor: Colors.deepPurple.shade200,
+        appBar: AppBar(
+          backgroundColor: Colors.deepPurple.shade800,
+          title: Text(
+            "DoToDo",
+            style: TextStyle(color: Colors.deepPurple.shade50),
+          ),
+          leading: Icon(Icons.menu, color: Colors.deepPurple.shade50),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.dark_mode, color: Colors.deepPurple.shade50),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.person, color: Colors.deepPurple.shade50),
+            ),
+          ],
+        ),
+        body: Container(
+          margin: EdgeInsets.only(top: 20),
           child: Column(
-            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.end,
+            spacing: 20,
             children: [
-              //Expanded can be used to make all items take equal space in given container by removing height and widhth from every children.
-              //box 1
-              Expanded( //expands to take all space in whatever dimention in missing i.e. height/width
-                child: Container(
-                  color: Colors.amber.shade200,
+              //task 1
+              Container(
+                height: 50,
+                width: 380,
+                margin: EdgeInsets.symmetric(horizontal: 20),
+                padding: EdgeInsets.symmetric(horizontal: 30),
+                color: Colors.deepPurple.shade400,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        'Task 1...',
+                        style: TextStyle(
+                          color: Colors.deepPurple.shade50,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 20,
+                      width: 20,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        color: Colors.deepPurple.shade50,
+                        border: Border.all(color: Colors.deepPurple.shade50),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              //box 2
-              Expanded(
-                flex: 3, //height ratio in comparison with other childrens
-                child: Container(
-                  color: Colors.amber.shade600,
+              //task 2
+              Container(
+                height: 50,
+                width: 380,
+                margin: EdgeInsets.symmetric(horizontal: 20),
+                padding: EdgeInsets.symmetric(horizontal: 30),
+                color: Colors.deepPurple.shade400,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        'Task 1...',
+                        style: TextStyle(
+                          color: Colors.deepPurple.shade50,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 20,
+                      width: 20,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        color: Colors.deepPurple.shade50,
+                        border: Border.all(color: Colors.deepPurple.shade50),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              //box 3
-              Expanded(
-                flex: 2,
-                child: Container(
-                  color: Colors.amber.shade800,
+              //task 3
+              Container(
+                height: 50,
+                width: 380,
+                margin: EdgeInsets.symmetric(horizontal: 20),
+                padding: EdgeInsets.symmetric(horizontal: 30),
+                color: Colors.deepPurple.shade400,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        'Task 1...',
+                        style: TextStyle(
+                          color: Colors.deepPurple.shade50,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 20,
+                      width: 20,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        color: Colors.deepPurple.shade50,
+                        border: Border.all(color: Colors.deepPurple.shade50),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
           ),
-        )
+        ),
+        bottomNavigationBar:
+        NavigationBar(destinations: const [
+          NavigationDestination(icon: Icon(Icons.home), label: "home"),
+          NavigationDestination(icon: Icon(Icons.add), label: "add"),
+          NavigationDestination(icon: Icon(Icons.logout), label: "logout")
+        ],
+        backgroundColor: Colors.deepPurple.shade100,),
       ),
     );
   }
